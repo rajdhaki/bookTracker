38d4214574bbe2e7d902f2cdfef6dfb2e44ddfed
RAJ DHAKI <rajdhaki5@gmail.com>
2025-06-23 10:47:38 +0530

    office system

--- server/src/controllers/registration.ts
+++ server/src/controllers/registration.ts
@@ -1,15 +1,17 @@
 import { prisma } from "@/app"
-
+import bcrypt from 'bcryptjs'
 
 export const registerUser = async (req, res) => {
 
     try {
         const userData = req.body
+        const hashPass = await bcrypt.hash(userData.password, 10)
+        console.log(hashPass)
         await prisma.user.create({
             data: {
                 name: userData.name,
                 email: userData.email,
-                password: userData.password
+                password: hashPass
             }
         })
         res.status(200).json(userData)
